8b77f40d88ceec66340221f9471d8bae55d37556
felipe gonzalez <gonzava@gmail.com>
2022-08-25 09:09:15 -0400

    reinitialized files

---
+++ src/main/java/cl/talentoDigital/M6C5D1miPrimerSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package cl.talentoDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M6C5D1miPrimerSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M6C5D1miPrimerSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+
+create table users(email varchar, password varchar, role varchar);
+
+insert into users(email, password, role) values('bobEsponja@mail.com','$2a$10$T4xQ89SRAZJbA5GrwtkwhuuY9dK4yCsEsu6FC5qyF.pjY2.WlC0gS','USER');
+
+insert into users(email, password, role) values ('patricioEstrella@mail.com','$2a$10$Ypnx5m1ocFKWpIHqhg013eb/pQvDTgFNMT1v3UEyKMmoCNe0ztn2S','ADMIN');
